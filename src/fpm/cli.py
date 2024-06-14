d896296f7d04b4ebbed8d1656755daa24f55edbd
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2024-06-15 01:28:24 +0200

    Clean up config file

--- src/fpm/cli.py
+++ src/fpm/cli.py
@@ -9,19 +9,20 @@ from fpm.generators.tasks import generate_task_specification
 from fpm.transformations.tasks import get_all_disinfection_tasks
 from fpm.transformations.objects import get_all_object_models, get_all_object_instances
 
+
 @click.group()
 def floorplan():
     pass
 
+
 def door_object_models(config, g):
-    output_folder = config["models"]["output"]
     template_path = config["templates"]["path"]
 
     object_models = get_all_object_models(g)
 
     for model in object_models:
         model_name = model["name"][5:]
-        output_path = os.path.join(output_folder, model_name)
+        output_path = get_output_path(config, "gazebo_models", model_name)
         generate_sdf_file(model, output_path, 
                        "model.sdf",
                        'gazebo/door.sdf.jinja', 
@@ -33,10 +34,8 @@ def door_object_models(config, g):
                         template_path=template_path
                        )
 
+
 def gazebo_world(config, g, model_name):
-    models_output_path = config["models"]["output"]
-    worlds_output_path = config["worlds"]["output"]
-    launch_output_path = config["launch"]["output"]
     template_path = config["templates"]["path"]
 
     instances = get_all_object_instances(g)
@@ -45,39 +44,49 @@ def gazebo_world(config, g, model_name):
         "name": model_name
     }
 
-    output_path = os.path.join(models_output_path, model_name)
-
+    output_path = get_output_path(config, "gazebo_models", model_name)
     generate_sdf_file(model, output_path,
                    "model.config",
                    "gazebo/model.config.jinja",
                     template_path=template_path
                    )
-
     generate_sdf_file(model, output_path,
                    "model.sdf",
                    "gazebo/floorplan.sdf.jinja",
                     template_path=template_path
                    )
 
-    generate_sdf_file(model, worlds_output_path,
+    output_path = get_output_path(config, "gazebo_worlds")
+    generate_sdf_file(model, output_path,
                    "{name}.world".format(name=model_name),
                    template_name="gazebo/world.sdf.jinja",
                     template_path=template_path
                    )
-    generate_launch_file(model_name, launch_output_path, 
+
+    output_path = get_output_path(config, "ros_launch")
+    generate_launch_file(model_name, output_path, 
                          template_name="ros/world.launch.jinja",
                          template_path=template_path
                          )
 
 
 def tasks(config, g, model_name):
-    output_path = config["points"]["output"]
-    inset_width = config["inset"]["width"]
+    output_path = get_output_path(config, "tasks", model_name)
+    inset_width = config["transformations"]["tasks"]["inset_width"]
 
     tasks = get_all_disinfection_tasks(g, inset_width)
-    directory = os.path.join(output_path, model_name)
     for task in tasks:
-        generate_task_specification(task, directory)
+        generate_task_specification(task, output_path)
+
+
+def get_output_path(config, model_type, model_name=None):
+    output_config = config.get("output")
+    subfolder = output_config.get(model_type)
+    if model_name is None:
+        return os.path.join(output_config["base_path"], subfolder)
+    else: 
+        return os.path.join(output_config["base_path"], subfolder, model_name)
+
 
 @floorplan.command()
 @click.argument("configfile", type=click.Path(exists=True, resolve_path=True))
@@ -92,8 +101,8 @@ def generate(configfile, inputs):
     tasks(config, g, model_name)
 
     door_object_models(config, g)
-
     gazebo_world(config, g, model_name)
+
 
 if __name__=="__main__":
     floorplan()
