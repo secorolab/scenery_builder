c173d21b5217cf0bb8c5b1a7c7003584696312fc
Argentina Ortega Sáinz <argenos@users.noreply.github.com>
2025-06-12 18:15:25 +0200

    Remove deg2rad conversion in transf. matrix

--- src/fpm/utils.py
+++ src/fpm/utils.py
@@ -53,12 +53,11 @@ def build_transformation_matrix(x, y, z, alpha):
     c = np.cos
     s = np.sin
 
-    a = np.deg2rad(alpha)
     t = np.array([[x], [y], [z], [1]])
     # fmt: off
     R = np.array([
-        [c(a), -s(a), 0],
-        [s(a), c(a), 0],
+        [c(alpha), -s(alpha), 0],
+        [s(alpha), c(alpha), 0],
         [0, 0, 1],
         [0, 0, 0]]
     )
